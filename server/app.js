7b511130c5063e51ae04220e36d6be22d54a64ca
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-10-30 15:32:09 +0100

    cors installed

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -13,33 +14,36 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
+
+app.use(cors());
+// app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
-app.get('/docs', (req, res) => {
-  res.sendFile(__dirname + '/views/docs.html');
+app.get("/docs", (req, res) => {
+  res.sendFile(__dirname + "/views/docs.html");
 });
 
-const cohorts = require(__dirname + '/data/cohorts.json');
+const cohorts = require(__dirname + "/data/cohorts.json");
 
-app.get('/api/cohorts', (req, res) => {
+app.get("/api/cohorts", (req, res) => {
   res.json(cohorts);
 });
 
-const students = require(__dirname + '/data/students.json');
+const students = require(__dirname + "/data/students.json");
 
-app.get('/api/students', (req, res) => {
+app.get("/api/students", (req, res) => {
   res.json(students);
 });
 
-app.get('*', (req, res) => {
-  res.status(404).json({ message: 'not found' });
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
 });
 
 // START SERVER
