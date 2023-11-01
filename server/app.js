57e8945c7dde3df6956e87f582b939e9ed58e0c5
Maria Lavrinenko <marylavrinenko@gmail.com>
2023-11-01 16:41:44 +0100

    routing for cohorts

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-require('dotenv').config({ path: './.env.sample' });
-require('./config/dbConnect');
+require("dotenv").config({ path: "./.env.sample" });
+require("./config/dbConnect");
 
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const Student = require('./models/Student.model');
-const Cohort = require('./models/Cohorts.model');
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohorts.model");
 
 // INITIALIZE EXPRESS APP
 const app = express();
@@ -16,43 +16,31 @@ const app = express();
 app.use(cors());
 // app.use(cors({ origin: ["http://localhost:5173"] }));
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use("/api/cohorts", require("./routes/cohort.routes"));
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 
-app.get('/docs', (req, res) => {
-  res.sendFile(__dirname + '/views/docs.html');
+app.get("/docs", (req, res) => {
+  res.sendFile(__dirname + "/views/docs.html");
 });
 
-app.get('/api/cohorts', (req, res) => {
-  Cohort.find({})
-    .then((cohorts) => {
-      console.log('Retrieved students ->', cohorts);
-      res.json(cohorts);
-    })
-    .catch((error) => {
-      console.error('Error while retrieving  ->', error);
-      res.status(500).send({ error: 'Failed to retrieve cohorts' });
-    });
-});
-
-app.get('/api/students', (req, res) => {
+app.get("/api/students", (req, res) => {
   Student.find({})
     .then((students) => {
-      console.log('Retrieved students ->', students);
+      console.log("Retrieved students ->", students);
       res.json(students);
     })
     .catch((error) => {
-      console.error('Error while retrieving  ->', error);
-      res.status(500).send({ error: 'Failed to retrieve students' });
+      console.error("Error while retrieving  ->", error);
+      res.status(500).send({ error: "Failed to retrieve students" });
     });
 });
 
-app.get('*', (req, res) => {
-  res.status(404).json({ message: 'not found' });
+app.get("*", (req, res) => {
+  res.status(404).json({ message: "not found" });
 });
 
 // START SERVER
